6b03fde49669c201b068a4dea688f88023802632
Jan Pražák <prazak.pracovni@gmail.com>
2024-11-30 18:53:35 +0100

    move stuff to transactins page

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,10 +1,45 @@
-import ManageTransactions from '@/features/transactions/components/ManageTransactions';
+import Button from '@/components/ui/Button';
+import { useGetCategories } from '@/features/categories/api/useGetCategories';
+import TransactionModal from '@/features/transactions/components/TransactionModal';
+import TransactionsHistory from '@/features/transactions/components/TransactionsHistory';
+import { ITransaction } from '@/features/transactions/interfaces/transaction.interface';
+import { useState } from 'react';
 
 const Transactions = () => {
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [transactionToEdit, setTransactionToEdit] = useState<ITransaction | undefined>(undefined);
+
+  const { data: categories } = useGetCategories();
+
+  const closeModal = () => {
+    setTransactionToEdit(undefined);
+    setIsModalOpened(false);
+  };
+
+  const openModal = (transactionToEdit?: ITransaction) => {
+    setTransactionToEdit(transactionToEdit);
+    setIsModalOpened(true);
+  };
+
+  if (!categories) {
+    return null;
+  }
+
   return (
-    <>
-      <ManageTransactions></ManageTransactions>
-    </>
+    <div className="flex flex-col gap-y-6 pt-8">
+      <div className="ml-auto">
+        <Button variant="primary" type="button" onClick={() => openModal(undefined)}>
+          + Add transaction
+        </Button>
+      </div>
+      <TransactionModal
+        categories={categories}
+        isOpened={isModalOpened}
+        transactionToEdit={transactionToEdit}
+        closeModal={closeModal}
+      />
+      <TransactionsHistory categories={categories} openModal={openModal} />
+    </div>
   );
 };
 
